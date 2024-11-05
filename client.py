e71f67fae88c60018c2e77a5ea88fbd77294bec4
Siya <siyasonpatki2@gmail.com>
2024-11-04 17:48:29 -0800

    added to help function

--- client.py
+++ client.py
@@ -59,7 +59,9 @@ def choiceThree():
 
 
 def choiceFour():
-    print("This is the user help page")
+    print('A bucket list program helps users manage, and track the goals they wish to accomplish.')
+    print('\nMy Bucket List provides an organized and interactive way to interact with items in your bucket list.')
+    print('\nMore Questions? Contact us at bucketlist/help@email.com')
 
 
 def main():
